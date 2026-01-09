12c7e8ae11bf64a9a7c065409daf05b7a94cf5a1
hionwi <tyunxiao@qq.com>
2026-01-09 19:47:29 +0800

    更新构建脚本，优化mnist_train编译选项，增加ffast-math和march=native参数

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -xe
 
 # clang -Wall -Wextra -o nn nn.c -lm -O3 && ./nn
-clang -Wall -Wextra -o mnist_train mnist_train.c -lm -O3 && ./mnist_train
+clang -Wall -Wextra -o mnist_train mnist_train.c -O3 -ffast-math -march=native -lm && ./mnist_train
